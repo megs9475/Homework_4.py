96f8fa6fd0b325e762b9d6e35a2bda80d460ac18
megs9475 <mlongua@uwyo.edu>
2024-11-13 04:45:59 +0000

    finished assignment

--- hw_4.py
+++ hw_4.py
@@ -1,1 +1,31 @@
+# Meghan Longua
+# UWYO COSC 1010
+# Submission Date: 11/12/2024
+# HW 04
+# Lab Section: 15
+# Sources, people worked with, help given to:
+# your
+# comments
+# here
 
+def read_file(input_file = 'prompt.txt'):
+    from pathlib import Path
+    path = Path(input_file)
+    contents = path.read_text()
+    lines = contents.splitlines()
+
+    for line in lines:
+        line = line.strip()
+        pairs = line.split("\t")
+        output_line = ""
+
+        for pair in pairs:
+            key, value = pair.split(":")
+            value = int(value)
+            if key == 'w':
+                output_line += " " * value
+            elif key == '*':
+                output_line += "*" * value
+        print(output_line)
+
+read_file()
